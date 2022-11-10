3cf446a25731b19189c029c55a88064779fcc591
bakelit69 <113536297+bakelit69@users.noreply.github.com>
2022-11-10 08:51:29 +0100

    Update StringTools.cs

--- Open-Lab-04.08/StringTools.cs
+++ Open-Lab-04.08/StringTools.cs
@@ -1,12 +1,16 @@
-﻿using System;
+using NUnit.Framework.Internal.Execution;
+using System;
+using System.Linq;
+using System.Diagnostics.Tracing;
 
+ 
 namespace Open_Lab_04._08
 {
     public class StringTools
     {
         public string[] IsFourLetters(string[] strings)
         {
-            throw new NotImplementedException();
+            return strings.Where(x=>x.Length == 4).ToArray();
         }
     }
 }
